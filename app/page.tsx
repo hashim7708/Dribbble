4bae571f530a443e220e1c59b9f9544443e7ee43
hashim7708 <mh6811172@gmail.com>
2023-06-30 00:37:36 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Catogory</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+    </div>
+  );
+}
